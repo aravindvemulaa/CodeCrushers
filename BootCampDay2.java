ab57d1f2091a8e09042b14c84ced547eae4a2e7d
aravindvemulaa <133363821+aravindvemulaa@users.noreply.github.com>
2023-05-16 21:09:47 +0530

    Add files via upload

---
+++ BootCampDay2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class BootCampDay2 {
+    public static void main(String args[]) {
+        System.out.println("Enter an Integer : ");
+        Scanner t= new Scanner(System.in);
+        int M=t.nextInt();
+        if(M%3==0&&M%5==0) {
+            System.out.println("Good Number");
+        }
+        else if(M%3==0) {
+            System.out.println("Bad Number");
+        }
+        else if(M%5==0) {
+            System.out.println("Poor Number");  
+        }
+        else 
+        {
+            System.out.println("-1");
+        }
+    }
+}
