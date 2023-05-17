f1e642f5081866f5afea1b375dcdf15719c6d0d4
aravindvemulaa <133363821+aravindvemulaa@users.noreply.github.com>
2023-05-17 22:36:39 +0530

    day3 Assignment Submitted

---
+++ BootCampDay3.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class BootCampDay3 {
+    public static void main(String args[]) {
+    int r,m=0,temp;
+     int n;
+    System.out.println("Enter a Number: ");
+        Scanner t=new Scanner(System.in);
+        n=t.nextInt();
+        temp=n;
+    while (n > 0) {
+            r=n%10;
+            m=(m*10)+r;
+            n=n/10;
+        }
+    if (temp==m) {
+       System.out.println("Palindrome Number");
+        } 
+    else {
+       System.out.println("Not a Palindrome Number");
+      }
+   }
+}
